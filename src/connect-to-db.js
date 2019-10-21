be78bb2a337fd06424c4f7240e756d0d6445dd77
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2019-10-21 11:48:43 +0700

    reusable connectToDb function

---
+++ src/connect-to-db.js
@@ -0,0 +1,21 @@
+// https://docs.atlas.mongodb.com/best-practices-connecting-to-aws-lambda/
+
+const mongoose = require('mongoose')
+
+let cachedDb = null
+
+function connectToDb() {
+  console.log(`connect to database `)
+
+  if (cachedDb) {
+    console.log(`using cached database`)
+    return Promise.resolve(cachedDb)
+  }
+
+  return mongoose.connect(process.env.REACT_APP_DB_URL).then(db => {
+    cachedDb = db
+    return cachedDb
+  })
+}
+
+module.exports = connectToDb
--- src/lambda/signup.js
+++ src/lambda/signup.js
@@ -1,25 +1,16 @@
 const { User } = require('../models')
-const mongoose = require('mongoose')
+const connectToDb = require('../connect-to-db')
 
 export async function handler(event, context) {
   context.callbackWaitsForEmptyEventLoop = false
 
   try {
-    const dbOptions = {
-      useNewUrlParser: true,
-      useFindAndModify: false,
-    }
-
-    mongoose.connect(process.env.REACT_APP_DB_URL, dbOptions)
-
+    connectToDb()
     const req = JSON.parse(event.body)
-
     const user = await User.create({
       email: req.email,
       password: req.password,
     })
-
-    console.log(user)
 
     return {
       statusCode: 200,
