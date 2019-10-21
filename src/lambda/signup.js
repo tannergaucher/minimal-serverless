a5005e0d53dc83fd9b50ca0478fb7d0ced5e9592
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2019-10-21 09:52:41 +0700

    db connect inside lambda func

--- src/lambda/signup.js
+++ src/lambda/signup.js
@@ -1,10 +1,17 @@
 const { User } = require('../models')
-const db = require('../db')
+const mongoose = require('mongoose')
 
 export async function handler(event, context) {
   context.callbackWaitsForEmptyEventLoop = false
 
   try {
+    const dbOptions = {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+    }
+
+    mongoose.connect(process.env.REACT_APP_DB_URL, dbOptions)
+
     const req = JSON.parse(event.body)
 
     const user = await User.create({
