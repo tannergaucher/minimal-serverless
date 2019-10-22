2baba2690154fb60ac207cde4cbd1dd6bb4cbc94
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2019-10-22 10:23:00 +0700

    handle loading state for signup request

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -6,57 +6,62 @@ import { IsAuthContext, UserContext } from '../context'
 export default function Signup() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [loading, setLoading] = useState(false)
   const { setIsAuth } = useContext(IsAuthContext)
   const { setUser } = useContext(UserContext)
 
   const history = useHistory()
 
   return (
-    <form
-      onSubmit={async e => {
-        e.preventDefault()
-        try {
-          const res = await fetch(`/.netlify/functions/signup`, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-              email,
-              password,
-            }),
-          })
+    <fieldset disabled={loading}>
+      <form
+        onSubmit={async e => {
+          e.preventDefault()
+          try {
+            setLoading(true)
+            const res = await fetch(`/.netlify/functions/signup`, {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({
+                email,
+                password,
+              }),
+            })
 
-          if (res.ok) {
-            const { data } = await res.json()
-            localStorage.setItem('token', data.token)
-            setIsAuth(true)
-            setUser(data.user)
-            history.push(`/`)
+            if (res.ok) {
+              const { data } = await res.json()
+              localStorage.setItem('token', data.token)
+              setIsAuth(true)
+              setLoading(false)
+              setUser(data.user)
+              history.push(`/`)
+            }
+          } catch (error) {
+            // TODO HANDLE ERROR
+            console.log(error)
           }
-        } catch (error) {
-          // TODO HANDLE ERROR
-          console.log(error)
-        }
-      }}
-    >
-      <input
-        type="email"
-        placeholder="email"
-        value={email}
-        onChange={e => setEmail(e.target.value)}
-        required
-      />
+        }}
+      >
+        <input
+          type="email"
+          placeholder="email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          required
+        />
 
-      <input
-        type="password"
-        placeholder="password"
-        value={password}
-        onChange={e => setPassword(e.target.value)}
-        required
-      />
+        <input
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          required
+        />
 
-      <button type="submit">Sign Up</button>
-    </form>
+        <button type="submit">Sign Up</button>
+      </form>
+    </fieldset>
   )
 }
